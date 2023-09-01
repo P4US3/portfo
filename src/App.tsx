47e24cd3b6c19b10ebc2b4d9f7b59401fa075efa
P4US3 <s27913@pjwstk.edu.pl>
2023-09-01 11:17:34 +0200

    idk

---
+++ src/App.tsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import './App.css';
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+      <>
+        <Navbar/>
+        <div className="App">
+          <header className="App-header">
+            <>
+              <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nulla turpis. Nunc varius
+                congue luctus. Cras quis aliquam diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus placerat sollicitudin fermentum. Phasellus blandit tortor vitae libero bibendum
+                convallis.</h1>
+              <br/>
+              Morbi et aliquam sapien. Morbi auctor mollis eros. Nunc accumsan ex leo, ac venenatis nibh varius et.
+              Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eleifend, turpis ac
+              auctor vehicula, ex enim scelerisque metus, sit amet pulvinar eros turpis eu lorem. Duis est massa,
+              pretium non ligula efficitur, ornare ullamcorper orci. Quisque fermentum pharetra ipsum, a placerat nibh
+              convallis sit amet. Vestibulum id nulla justo. Aliquam ultricies, arcu ac suscipit pulvinar, tortor tellus
+              faucibus lacus, vel condimentum sapien libero vel nulla. Vivamus scelerisque iaculis nisi eu vestibulum.
+              Donec porta, ante ut elementum gravida, elit nunc hendrerit ex, vitae imperdiet sem turpis eu lorem.
+              <br/>
+              In fermentum diam ut augue eleifend aliquam. Sed vulputate elit ac sapien venenatis, sed eleifend leo
+              sollicitudin. Praesent mollis elit eros, vel gravida magna luctus id. Fusce ut accumsan ligula. Aliquam
+              aliquet lorem metus, sit amet eleifend urna pharetra id. Pellentesque sed nisl tempor, egestas purus non,
+              porttitor libero. Suspendisse eleifend nulla nec laoreet cursus. Cras tempus dui id ligula auctor
+              fringilla. Morbi a magna a tortor egestas porttitor et eu libero. Ut consectetur justo vitae nisl dapibus
+              pretium facilisis at metus. Duis sit amet velit elementum, convallis elit et, molestie diam. Cras nec
+              metus eget ligula scelerisque finibus sed at lectus. Nullam sed feugiat eros. Nulla facilisi.
+
+            </>
+          </header>
+          <div/>
+
+          <div className="row">
+            <div className="column">
+              <img className="Def-images" src="https://loremflickr.com/320/240"/>
+            </div>
+            <div className="column">
+              <img className="Def-images" src="https://loremflickr.com/320/240"/>
+            </div>
+            <div className="column">
+              <img className="Def-images" src="https://loremflickr.com/320/240"/>
+            </div>
+          </div>
+
+          <body className="App-body">
+          <>
+
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus rutrum mi sem. Vivamus quis metus non
+            metus volutpat congue vel ultricies felis. Aliquam erat volutpat. Etiam posuere urna id pharetra suscipit.
+            Etiam sed risus metus. Aenean non gravida orci. Maecenas finibus sapien id placerat fringilla. Suspendisse
+            in lacus at ante mattis maximus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis
+            nulla turpis. Nunc varius congue luctus. Cras quis aliquam diam. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Phasellus placerat sollicitudin fermentum. Phasellus blandit tortor vitae libero bibendum
+            convallis.
+            <br/>
+            Morbi et aliquam sapien. Morbi auctor mollis eros. Nunc accumsan ex leo, ac venenatis nibh varius et.
+            Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eleifend, turpis ac
+            auctor vehicula, ex enim scelerisque metus, sit amet pulvinar eros turpis eu lorem. Duis est massa, pretium
+            non ligula efficitur, ornare ullamcorper orci. Quisque fermentum pharetra ipsum, a placerat nibh convallis
+            sit amet. Vestibulum id nulla justo. Aliquam ultricies, arcu ac suscipit pulvinar, tortor tellus faucibus
+            lacus, vel condimentum sapien libero vel nulla. Vivamus scelerisque iaculis nisi eu vestibulum. Donec porta,
+            ante ut elementum gravida, elit nunc hendrerit ex, vitae imperdiet sem turpis eu lorem.
+            <br/>
+            In fermentum diam ut augue eleifend aliquam. Sed vulputate elit ac sapien venenatis, sed eleifend leo
+            sollicitudin. Praesent mollis elit eros, vel gravida magna luctus id. Fusce ut accumsan ligula. Aliquam
+            aliquet lorem metus, sit amet eleifend urna pharetra id. Pellentesque sed nisl tempor, egestas purus non,
+            porttitor libero. Suspendisse eleifend nulla nec laoreet cursus. Cras tempus dui id ligula auctor fringilla.
+            Morbi a magna a tortor egestas porttitor et eu libero. Ut consectetur justo vitae nisl dapibus pretium
+            facilisis at metus. Duis sit amet velit elementum, convallis elit et, molestie diam. Cras nec metus eget
+            ligula scelerisque finibus sed at lectus. Nullam sed feugiat eros. Nulla facilisi.
+            <br/>
+            Vestibulum mollis lobortis mi, vitae pretium mauris feugiat in. Vivamus pretium posuere tellus. Cras ac
+            massa mauris. Proin auctor fringilla arcu, sit amet elementum erat tincidunt ut. Phasellus at viverra lorem,
+            ut fringilla nibh. Etiam sodales purus eu condimentum pellentesque. Donec bibendum maximus pellentesque.
+            <br/>
+            Fusce scelerisque sem lorem, eget sodales odio aliquam quis. Vestibulum mollis aliquam felis, sit amet
+            commodo tortor ultrices id. Morbi congue condimentum congue. In hac habitasse platea dictumst. Etiam sed
+            congue justo. Aenean mattis ante urna, quis faucibus tellus commodo id. Etiam non lacus dapibus, dignissim
+            mauris nec, tempor enim.
+            <br/>
+            Curabitur ac lectus orci. Phasellus quis vulputate dolor. Ut sagittis ligula at mauris sollicitudin, quis
+            euismod augue lobortis. Aliquam sed semper ex. Duis eleifend erat a enim sollicitudin fermentum. Sed a
+            tempus arcu. Vestibulum quis elit sapien. Proin sollicitudin ipsum non eleifend volutpat. Nullam tincidunt
+            erat at lectus gravida, in rhoncus nibh pulvinar.
+            <br/>
+            Cras elementum nisl nec facilisis suscipit. Integer nec urna sit amet ipsum placerat volutpat sit amet non
+            ante. Duis condimentum risus aliquet velit bibendum, a bibendum leo sagittis. Phasellus tempor, nunc nec
+            sollicitudin iaculis, nunc tellus sagittis leo, tempor faucibus turpis ex at lacus. Suspendisse sit amet
+            justo ac turpis dictum porttitor. In pellentesque sagittis augue, eget tempus metus laoreet sit amet. Nullam
+            non elit ipsum.
+            <br/>
+            Pellentesque aliquet mi ligula, vitae accumsan felis tincidunt at. Maecenas tincidunt mauris non ultrices
+            congue. In ac dui laoreet, ultricies mauris at, placerat sem. Sed volutpat, odio vel laoreet laoreet, mi
+            tellus tempus sem, ac posuere mauris ipsum ac elit. Curabitur vehicula lectus neque, nec sodales massa
+            pharetra quis. In varius est vel est posuere aliquet vel in augue. Vestibulum diam risus, rhoncus sed velit
+            at, efficitur sodales augue. Suspendisse laoreet semper libero et laoreet. Suspendisse pulvinar neque
+            tellus, in suscipit sapien iaculis ut. Curabitur vel neque sed metus egestas molestie nec sed augue. Integer
+            porttitor turpis efficitur vehicula porta.
+          </>
+          </body>
+          <div className="row">
+            <div className="column">
+              <a className="Side-text">
+              In fermentum diam ut augue eleifend aliquam. Sed vulputate elit ac sapien venenatis, sed eleifend leo
+              sollicitudin. Praesent mollis elit eros, vel gravida magna luctus id. Fusce ut accumsan ligula. Aliquam
+              aliquet lorem metus, sit amet eleifend urna pharetra id. Pellentesque sed nisl tempor, egestas purus non,
+              porttitor libero. Suspendisse eleifend nulla nec laoreet cursus. Cras tempus dui id ligula auctor fringilla.
+              Morbi a magna a tortor egestas porttitor et eu libero. Ut consectetur justo vitae nisl dapibus pretium
+              facilisis at metus. Duis sit amet velit elementum, convallis elit et, molestie diam. Cras nec metus eget
+              ligula scelerisque finibus sed at lectus. Nullam sed feugiat eros. Nulla facilisi.
+                <br/>
+                Pellentesque aliquet mi ligula, vitae accumsan felis tincidunt at. Maecenas tincidunt mauris non ultrices
+                congue. In ac dui laoreet, ultricies mauris at, placerat sem. Sed volutpat, odio vel laoreet laoreet, mi
+                tellus tempus sem, ac posuere mauris ipsum ac elit. Curabitur vehicula lectus neque, nec sodales massa
+                pharetra quis. In varius est vel est posuere aliquet vel in augue. Vestibulum diam risus, rhoncus sed velit
+                at, efficitur sodales augue. Suspendisse laoreet semper libero et laoreet. Suspendisse pulvinar neque
+                tellus, in suscipit sapien iaculis ut. Curabitur vel neque sed metus egestas molestie nec sed augue.
+              </a>
+            </div>
+            <div className="column-img">
+              <img className="Side-img" src="https://loremflickr.com/320/240"/>
+            </div>
+          </div>
+          <div className="App-foot">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus rutrum mi sem. Vivamus quis metus non
+            metus volutpat congue vel ultricies felis. Aliquam erat volutpat. Etiam posuere urna id pharetra suscipit.
+            Etiam sed risus metus. Aenean non gravida orci. Maecenas finibus sapien id placerat fringilla. Suspendisse
+            in lacus at ante mattis maximus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum nisl nec facilisis suscipit. Integer nec urna sit amet ipsum placerat volutpat sit amet non
+            ante. Duis condimentum risus aliquet velit bibendum, a bibendum leo sagittis. Phasellus tempor, nunc nec
+            sollicitudin iaculis, nunc tellus sagittis leo, tempor faucibus turpis ex at lacus. Suspendisse sit amet
+            justo ac turpis dictum porttitor. In pellentesque sagittis augue, eget tempus metus laoreet sit amet. Nullam
+            non elit ipsum.
+          </div>
+        </div>
+      </>
+
+  );
+}
+
+export default App;
---
+++ src/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return <nav className="nav">
+        <a href="/" className="site-title">Site Name</a>
+        <ul>
+
+            <li><a className="option" href="/Modelling">3D Modeling</a></li>
+            <li><a className="option" href="/Coding">Coding</a></li>
+            <li><a className="option" href="/Design">Design</a></li>
+
+        </ul>
+    </nav>
+
+}
